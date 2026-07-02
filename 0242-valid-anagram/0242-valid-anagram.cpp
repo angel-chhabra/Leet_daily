0b5b9f364df791a171768f140d627e9dd6ad77a2
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-02 10:27:04 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
-        if(s==t){
-            return true;
-        } else{
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
+        // if(s==t){
+        //     return true;
+        // } else{
+        //     return false;
+        // }
+
+        if(s.length()!=t.length()){
             return false;
         }
-        
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true; 
     }
 };
