11f27d278cbdebfb2573432b96aa696954d6001d
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-07 13:10:10 +0530

    Time: 78 ms (68.59%), Space: 88.9 MB (49.67%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,14 +3,13 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> set;
         int maxLength=0;
-        int length=1;
         for(int i: nums){
             set.insert(i);
         }
         for(int i: set){
             if(set.find(i-1)==set.end()){
                 int a=i;
-                length=1;
+                int length=1;
                 while(set.find(a+1)!=set.end()){
                     length++;
                     a++;
