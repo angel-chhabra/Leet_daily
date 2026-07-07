fee9c6af731c2a48e5055b7ad4101e3f85da43e3
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-07 13:46:16 +0530

    Time: 0 ms (100%), Space: 28.2 MB (32.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int majorityElement;
-        unordered_map<int , int> map;
-        for(auto i: nums){
-            map[i]++;
-        }
-        for(auto i: map){
-            if(i.second>n/2){
-                majorityElement=i.first;
+        int count=0;
+        int candidate=0;
+        for(int i: nums){
+            if(count==0){
+                candidate=i;
+            }
+            if(i==candidate){
+                count++;
+            } else{
+                count--;
             }
         }
-        return majorityElement;
+        return candidate;
+
+
+
+
+        // int n=nums.size();
+        // int majorityElement;       //learn its algorithm also , moore voting algorithm
+        // unordered_map<int , int> map;
+        // for(auto i: nums){
+        //     map[i]++;
+        // }
+        // for(auto i: map){
+        //     if(i.second>n/2){
+        //         majorityElement=i.first;
+        //     }
+        // }
+        // return majorityElement;
         
     }
 };
