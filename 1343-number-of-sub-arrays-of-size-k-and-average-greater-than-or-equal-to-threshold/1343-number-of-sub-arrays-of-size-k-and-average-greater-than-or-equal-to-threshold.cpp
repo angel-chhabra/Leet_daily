69fcfb953a379463f229732be05c6d79961ef117
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-08 13:14:14 +0530

    Time: 0 ms (100%), Space: 59.3 MB (38.36%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -3,7 +3,7 @@ public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
         int n=arr.size();
         int wsum=0;
-        int count=0;
+        int count=0;   //count create kia taaki hr window jiska avg bda h threshold se uske lie count++ krlia
         for(int i=0;i<k;i++){
             wsum+=arr[i];
         }
@@ -12,7 +12,7 @@ public:
         }
         int i=0;
         for(int j=k;j<n;j++){
-            wsum=wsum+arr[j]-arr[i];
+            wsum=wsum+arr[j]-arr[i];    //window slide hori h by 1 
             if(wsum>=threshold*k){
                 count++;
             }
