856a77692f833c2b63ab3a85f105dcaea4abbb90
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-08 13:58:40 +0530

    Time: 0 ms (100%), Space: 41.9 MB (15.91%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int minisize=INT_MAX;
+        int minisize=INT_MAX;   //to maintain the min length of subarray
         int wsum=0;
         int n=nums.size(); 
         int j=0;
-        for(int i=0;i<n;i++){
-            wsum+=nums[i];
+        for(int i=0;i<n;i++){  
+            wsum+=nums[i];     //window calculate krte rhnge ek ek element add kr kr ke
             while(wsum>=target){
-                int size=i-j+1;
-                minisize=min(size , minisize);
+                int size=i-j+1;    //size of window jab bhi uska wsum >=target ho 
+                minisize=min(size , minisize);    
 
-                wsum=wsum-nums[j];
-                j++;
+                wsum=wsum-nums[j];    //aage bdhna , kyuki aage bhi scope h or bhi chhota subarr milne ka w wsum>=target
+                j++;                 //window ko aage bdhane k lie and first elemnt ko subtract krne k lie
             }
         }
         if(minisize==INT_MAX){
