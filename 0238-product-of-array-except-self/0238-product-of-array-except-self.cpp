a98257a993a159bea15c795739d72cc4b8b95725
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-09 12:59:13 +0530

    Time: 1 ms (50.01%), Space: 40.1 MB (96.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -9,7 +9,7 @@ public:
         int rightproduct=1;
         for(int i=n-1;i>=0;i--){           //suffix product
             ans[i]=ans[i]*rightproduct;
-            rightproduct*=nums[i];       //kyuki hme nums[i] se multiply nhi krte rhna , hme hr bar jitne b nums[i] right m aate rhnge as loop goes forward , sbka right product chahiye hme
+            rightproduct*=nums[i];       //kyuki hme nums[i] se multiply nhi krte rhna , hme hr bar jitne b nums[i] right m aate rhnge as loop goes forward , sbka right product chahiye hme and kyuki ans array m already suffix product store ho chuke h from 1 to n to ab n to 0 hmara dubara ans m nhi store kr skte vrna values overwrite
         }
         return ans;
         
