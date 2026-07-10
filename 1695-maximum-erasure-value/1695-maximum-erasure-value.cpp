f8fecd2b35847e0dbe7b286ca941888e1c78498f
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-10 13:34:27 +0530

    Time: 7 ms (95%), Space: 93.6 MB (97.91%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,17 +4,17 @@ public:
         int n=nums.size();
         int wsum=0;
         int maxi=INT_MIN;
-        int freq[10001]={0};
+        int freq[10001]={0};     //kyuki int 10000 tak ja skte h and index fr 9999 tak hi jayega so 10001 size 
         int j=0;
         for(int i=0;i<n;i++){
-            freq[nums[i]]++;
-            wsum+=nums[i];
-            while(freq[nums[i]]>1){
+            freq[nums[i]]++;    //hr number ki freq calculate krte rho
+            wsum+=nums[i];       //and sbka sum kyuki sum dena h ans m
+            while(freq[nums[i]]>1){   //jse hi freq 1 se bdhti h kisi b num ki , window shrink krnge hm and freq--
                 wsum=wsum-nums[j];
                 freq[nums[j]]--;
                 j++;
             }
-            maxi=max(maxi , wsum);
+            maxi=max(maxi , wsum);    //max sum ke lie
         }
         return maxi;
         
