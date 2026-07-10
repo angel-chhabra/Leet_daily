bfc1e7282f6209d1c69cec19c888f4aec16e8a9f
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-10 12:54:02 +0530

    Time: 0 ms (100%), Space: 69.5 MB (93.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -7,16 +7,16 @@ public:
         int j=0;
         vector<int> ans;
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
+            if(nums[i]==0){      //agr right m zero aari h to zeroes++ hoga 
                 zeroes++;
             }
-            while(zeroes>k){
+            while(zeroes>k){    //jse hi zeroes bdgyi k se , to hm window shrink krna start krnge , or agr shrink krte krte left me 0 aya to zeroes ka count b decrease krnge
                 if(nums[j]==0){
                     zeroes--;
                 }
-                j++;
+                j++; 
             }
-            count=max(count , i-j+1);
+            count=max(count , i-j+1);    //max of count and jo possible subarray size hoga 
 
         }
         return count;  
