d99c6934260f00dcb98f2d44ae646304130f4584
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-13 11:33:56 +0530

    Time: 13 ms (37%), Space: 12.6 MB (37%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -5,18 +5,18 @@ public:
         int maxi=-1;
         unordered_map<string ,int> map;
         for(int i=0;i<s.size();i++){
-            string str="";
+            string str="";     //hr type ki substr check krte jaynge hr s[i] ko add krte krte
             for(int j=i;j<s.size();j++){
-                if(s[i]==s[j]){
+                if(s[i]==s[j]){    //agr chars same same h to str m plus kro coz we want special substr
                     str+=s[i];
-                    map[str]++;
+                    map[str]++;  //hr us special substr ki freq store kro in map
             } else{
                 break;
             }
             }
         }
             for(auto [k,v]:map){
-                if(v>=3){
+                if(v>=3){     // map traverse krte hue jis b substr ki freq>=3 hogi uski length lenge and check w maxi
                     length =(int)k.size();
                     maxi=max(length , maxi);
                 }
