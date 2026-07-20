634fb950aef535e215601bc12c2e1103fc3efedb
Angel Chhabra <chhabraangel23@gmail.com>
2026-07-20 13:55:30 +0530

    Time: 0 ms (100%), Space: 19.7 MB (9.1%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,23 +11,26 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      ListNode* result = new ListNode(-1); 
-      if(l1 == NULL)return l2;
-      if(l2 == NULL)return l1; 
-      ListNode* temp =result;
-      while(l1 != NULL && l2 !=NULL){
-        if(l1->val < l2->val){
-            temp->next = new ListNode(l1->val);
-            l1 = l1->next;
-            temp = temp->next;
-        }else{
-            temp->next = new ListNode(l2->val);
-            l2 = l2->next;
-            temp = temp->next;
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        ListNode * result=new ListNode(-1);
+        ListNode*temp=result;  //result ka head preserve hogya 
+      while(l1!=NULL && l2!=NULL){
+        if(l1->val<l2->val){  //jiski b value choti hogi , uske lie new node bnajayegi and result m add hojegi
+            temp->next=new ListNode(l1->val);
+            l1=l1->next;  //next hojayega fr uska b and temp ka b
+            temp=temp->next;
+        } else{
+            temp->next=new ListNode(l2->val);
+            l2=l2->next;
+            temp=temp->next;
         }
       }
-      if(l1 != NULL) temp->next = l1;
-      if(l2 != NULL) temp->next =l2;
-      return result->next;
+        if(l1!=NULL){   //agr kuch bch jyega to use b add krna pdega
+            temp->next=l1;
+        } if(l2!=NULL){
+            temp->next=l2;
+        }
+        return result->next;   //bcoz starting m to -1 h and uske bad se list start h
     }
 };
